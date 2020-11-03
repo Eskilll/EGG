0e51713a7cf036513710a3b4587ca9b21e76142b
Eskilll <eskil.nordlander@elev.ga.ntig.se>
2020-11-03 12:39:49 +0100

    made all of program.cs work

--- EGG/Program.cs
+++ EGG/Program.cs
@@ -9,8 +9,36 @@ namespace EGG
             EGG EGG = new EGG();
             System.Console.WriteLine("Name Your EGG!");
             EGG.name = System.Console.ReadLine();
-            
+            string inpt = ""; 
+            bool live = true;
+            while(live == true){
+                System.Console.WriteLine("Feed,Hi,Teach,Stats");
+                inpt = System.Console.ReadLine();
+                if(inpt == "Feed"){
+                    EGG.Feed();
+                    inpt = "";
+                }
+                else if(inpt == "Hi"){
+                    EGG.Hi();
+                    inpt = "";
+                }
+                else if(inpt == "Teach"){
 
+                    inpt = "";
+                }
+                else if(inpt == "Stats"){
+                    EGG.PrintStats();
+                    inpt = "";
+                }
+                else{
+                    System.Console.WriteLine("Sorry didn't quite catch that try something else!");
+                }
+
+                EGG.Tick();
+
+                live = EGG.GetAlive();
+
+            }
 
 
         }
